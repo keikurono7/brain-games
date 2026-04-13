3f207d4bafa62da320ac8b5c41b1defcaf42408b
madhu <dmpathani@gmail.com>
2026-04-13 11:11:39 +0530

    changed image loading

--- src/games/card.jsx
+++ src/games/card.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './games.css'
 
 function Card() {
   const [page, setPage] = React.useState("start");
-
-  const cards = ["JBC","JBS","JRD","JRH","KBC","KBS","KRD","KRH","QBC","QBS","QRD","QRH"];
   const [setA, setSetA] = React.useState([]);
   const [setB, setSetB] = React.useState([]);
+  const [loaded, setLoaded] = React.useState(false);
+
+  const cards = ["JBC","JBS","JRD","JRH","KBC","KBS","KRD","KRH","QBC","QBS","QRD","QRH"];
+
+  useEffect(() => {
+    let loadedCount = 0;
+
+    cards.forEach(card => {
+      const img = new Image();
+      img.src = `/assets/${card}.png`;
+      img.onload = () => {
+        loadedCount++;
+        if (loadedCount === cards.length) {
+          setLoaded(true);
+        }
+      };
+    });
+  }, []);
 
   const shuffleCards = () => {
     for (let i = cards.length - 1; i > 0; i--) {
@@ -27,16 +43,17 @@ function Card() {
   } 
 
   const start = () => {
+    let tempCards = [...cards];
     let tempA = [];
     let tempB = [];
     do{
       shuffleCards();
-      tempA = cards.slice(0, 6).map(card => card);
-      tempB = cards.slice(6, 11).map(card => card);
+      tempA = tempCards.slice(0, 6).map(card => card);
+      tempB = tempCards.slice(6, 11).map(card => card);
       console.log(tempA[0]);
     }while(!cardCheck(tempA) || !cardCheck(tempB));
-    setSetA(cards.slice(0, 6));
-    setSetB(cards.slice(6, 11));
+    setSetA(tempA);
+    setSetB(tempB);
     setPage("game");
     setTimeout(() => {
       setPage("game2");
@@ -47,6 +64,10 @@ function Card() {
     setTimeout(() => {
       setPage("end");
     }, 6000);
+  }
+
+  if (!loaded) {
+    return <p>Loading...</p>;
   }
 
   return (
